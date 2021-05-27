f9fc72bf19c25d093273323dfa5fac396c748bd3
kubson991 <jpoveda311@unab.edu.co>
2021-05-26 19:18:33 -0500

    se terminaron las tablas del section 1

--- scripts/main.js
+++ scripts/main.js
@@ -1,18 +1,14 @@
 const tabla = document.getElementById("section1")
-
-console.log(tabla)
-
 function cambiarAcomisiones(){
-    console.log("hizo algo")
-    tabla.innerHTML=`<button class="conversion-de-moneda__botonTabla" onclick="cambiarAcomisiones()"></button>
-    <h4>Comisiones</h4>
+    tabla.innerHTML=`
+    <h4 class= "conversion-de-moneda__tituloComisiones"> Comisiones </h4>
     <table>
         <tr>
             <td>Bitrade</td>
             <td>$ 12.96</td>
           </tr>
           <tr>
-            <td>Ethereum</td>
+            <td>Bitpreco</td>
             <td>$ 13.07</td> 
           </tr>
           <tr>
@@ -24,5 +20,32 @@ function cambiarAcomisiones(){
             <td>$ 14.96 </td>
           </tr>
     </table>
-    <div class="tasas-de-cambio__actualizacion"><b>Actualizado:</b><p> 19 Julio 23:45</p></div>`
+    <button class="conversion-de-moneda__botonTabla izquierda" onclick="cambiarAmonedas()"></button>
+    <div class="tasas-de-cambio__actualizacion comisiones"><b>Actualizado:</b><p> 19 Julio 23:48</p></div>`
+}
+
+ function cambiarAmonedas(){
+    tabla.innerHTML= 
+     `<h4 class="conversion-de-moneda__titulomoneda">Monedas</h4>
+     <table>
+         <tr>
+             <td>Bitcoin</td>
+             <td>$ 1.96 <span class="conversion-de-moneda__decrecimientoTabla"></span></td>
+           </tr>
+           <tr>
+             <td>Ethereum</td>
+             <td>$ 0.07 <span class="conversion-de-moneda__crecimientoTabla"></span></td> 
+           </tr>
+           <tr>
+             <td>Ripple</td>
+             <td>$ 2.15 <span class="conversion-de-moneda__decrecimientoTabla"></span></td>
+           </tr>
+           <tr>
+             <td>Stellar</td>
+             <td>$ 4.96 <span class="conversion-de-moneda__decrecimientoTabla"></span></td>
+           </tr>
+     </table>
+     <button class="conversion-de-moneda__botonTabla derecha" onclick="cambiarAcomisiones()"></button>
+     <div class="tasas-de-cambio__actualizacion"><b>Actualizado:</b><p> 19 Julio 23:45</p></div>`
+
 }
